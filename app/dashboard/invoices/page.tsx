00fa3369719ec378d150cd41bdcebe9d655dc3fe
RuthAdora <adoraruth01@hmail.com>
2025-05-19 12:37:26 +0300

    first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,15 @@
+// /dashboard/invoices/page.tsx
+import React from "react";
+
+interface Props {}
+
+const InvoicesPage: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Invoices</h1>
+      {/* Your invoices content here */}
+    </div>
+  );
+};
+
+export default InvoicesPage;
